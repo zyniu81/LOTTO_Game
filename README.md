35a982530647c2cc8c499e12f65c4afe43d4619e
zyniu81@wp.pl <zyniu81>
2024-01-19 15:32:48 +0100

    Second version

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # LOTTO_Game
+
+It is a simple game in which the player selects six numbers from 1 to 49, and then the computer draws the numbers and checks whether the player got the numbers right. The player wins if he matches 3 to 6 numbers.
+
+## Requirements
+
+- Python 3.x
+
+## Usage
+
+1. Clone the repository.
+2. Open the file `guessing_game.py` in your Python IDE (such as PyCharm).
+3. Run the file.
+4. Follow the instructions on the screen to play the game.
+
+## Author
+
+zyniu81
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
--- lotto_game.py
+++ lotto_game.py
@@ -2,10 +2,13 @@ import random
 
 
 def lotto_game():
+    """Generates random numbers and allows the user to guess which ones will be drawn.
+
+      Raises:
+          ValueError: If the user enters a number outside the range of 1-49.
+
+      """
     my_number = []
-    lotto_list = list(range(50)[1:50])
-    random.shuffle(lotto_list)
-    lotto_number = lotto_list[0:6]
     while len(my_number) < 6:
         number = input("Enter your number between 1 and 49: ")
         try:
@@ -21,8 +24,24 @@ def lotto_game():
             print("You have already provided this number!")
             continue
 
-    print(sorted(my_number))
-    print(sorted(lotto_number))
+    lotto_list = list(range(50)[1:50])
+    random.shuffle(lotto_list)
+    lotto_number = lotto_list[0:6]
+    result = [i for i in my_number if i in lotto_number]
+    if len(result) == 3:
+        print("You guessed three numbers.")
+    elif len(result) == 4:
+        print("You guessed four numbers.")
+    elif len(result) == 5:
+        print("You guessed five numbers.")
+    elif len(result) == 6:
+        print("You guessed six numbers.")
+    else:
+        print("Sorry. You lost.")
+
+    print(*result)
+    print(f"Your entered numbers:", *sorted(my_number))
+    print("Numbers drawn:", *sorted(lotto_number))
 
 
 lotto_game()
